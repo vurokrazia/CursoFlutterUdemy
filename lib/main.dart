fc40debaef64770cec9e1212992d29c94cb08ffd
vurokrazia <jesus.alberto.vk@gmail.com>
2019-06-19 23:18:39 -0500

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:prueba2/src/pages/home_page.dart';
+ 
+void main() => runApp(MyApp());
+ 
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Material App',
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+      )
+    ;
+  }
+}
